671765023220007e9da92472712ae1821306b8e6
wwhfrank <wwhfranky@gmail.com>
2021-10-10 17:56:52 -0700

    make turret's barrel keep tracking enemy's movement

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public List<GameObject> enemys = new List<GameObject>();
+	public List<GameObject> attackList = new List<GameObject>();
     void OnTriggerEnter(Collider col) {
         if (col.tag == "Enemy") {
-            enemys.Add(col.gameObject);
+			attackList.Add(col.gameObject);
         }
     }
 
     void OnTriggerExit(Collider col) {
         if (col.tag == "Enemy") {
-            enemys.Remove(col.gameObject);
+			attackList.Remove(col.gameObject);
         }
     }
 
     //多少秒攻击一次
-    public float attackRateTime = 2;
+	public float bulletLoadingTime = 2;
     public float timer = 0;
     public List<GameObject> bulletPrefabList = new List<GameObject>();
     public Transform firePosition;
@@ -26,50 +26,49 @@ public class Turret : MonoBehaviour
     public Transform head;
 
     void Start() {
-        timer = attackRateTime;
+		timer = bulletLoadingTime;
     }
 
 
     void Update() {
-        timer += Time.deltaTime;
-        // 生成子弹的逻辑也要改为集合中存在未被锁定的敌人
-        if (enemys.Count > 0 && timer >= attackRateTime) {
-            bool flag = Attack();
-            if (flag) {
-                timer = 0;
-            }
-        }
+		RemoveNull();
 
+		if (timer < bulletLoadingTime) {
+			timer += Time.deltaTime;
+		}
+
+
+		// 指定Enemy为集合当中第一个未被锁定的
+		GameObject enemy = null;
+		for (int i = 0; i < attackList.Count; ++i) {
+			GameObject temp = attackList[i];
+			bool isUnlocked = temp.GetComponent<Enemy>().CheckStatus();
+			if (isUnlocked) {
+				enemy = attackList[i];
+				break;
+			}
+		}
+
+		if (enemy != null) {
+			Vector3 targetPosition = enemy.transform.position;
+			targetPosition.y = head.position.y;
+			head.LookAt(targetPosition);
+
+			// 生成子弹的逻辑也要改为集合中存在未被锁定的敌人
+			if (timer >= bulletLoadingTime) {
+				bool attackSuccess = Attack(enemy);
+				if (attackSuccess) {
+					timer = 0;
+				}
+			}
+		}
     }
 
     // 把伤害改为染色操作
-
-    bool Attack() {
-        // 确保整个enemys中没有为null的
-        UpdateEnemys();
-        if (enemys.Count == 0) {
-            timer = attackRateTime;
-            return false;
-        }
-
-        // 指定Enemy为集合当中第一个未被锁定的
-        GameObject enemy = null;
-        for (int i = 0; i < enemys.Count; ++i) {
-            GameObject temp = enemys[i];
-            bool flag = temp.GetComponent<Enemy>().CheckStatus();
-            if (flag) {
-                enemy = enemys[i];
-                break;
-            }
-        }
-
-        if (enemy == null) {
-            return false;
-        }
-
-        Vector3 targetPosition = enemy.transform.position;
-        targetPosition.y = head.position.y;
-        head.LookAt(targetPosition);
+	bool Attack(GameObject enemy) {
+		if (enemy == null) {
+			return false;
+		}
 
         // 可以设置不同的bullet的prefab，搞成一个list去在里面遍历
         GameObject bullet = GameObject.Instantiate(bulletPrefabList[times], firePosition.position, firePosition.rotation);
@@ -82,16 +81,14 @@ public class Turret : MonoBehaviour
 
     }
 
-    void UpdateEnemys() {
-        List<int> emptyList = new List<int>();
-        for (int i = 0; i < enemys.Count; ++i) {
-            if (enemys[i] == null) {
-                emptyList.Add(i);
-            }
-        }
-
-        for (int i = 0; i < emptyList.Count; ++i) {
-            enemys.RemoveAt(emptyList[i] - i);
-        }
+	void RemoveNull() {
+		int i = 0;
+		while (i < attackList.Count) {
+			if (attackList [i] == null) {
+				attackList.RemoveAt (i);
+			} else {
+				i++;
+			}
+		}
     }
 }
